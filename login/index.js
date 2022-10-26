0889c1f61cdfbd08a3d4a1122e5c9d7be2fe88b2
GabrielRen21 <104105791+GabrielRen21@users.noreply.github.com>
2022-10-26 04:24:54 -0300

    Update index.js

--- login/index.js
+++ login/index.js
@@ -12,6 +12,22 @@ const kafka = new Kafka({
     brokers: [process.env.kafkaHost]
 });
 
+const topicParticiones  = async () => {
+    const admin = kafka.admin();
+    await admin.connect();
+    await admin.createTopics({
+        topics: [
+        	{ topic: 'nuevosMiembros', numPartitions: 2 },
+        	{ topic: 'ventas', numPartitions: 2 },
+        	{ topic: 'stock', numPartitions: 2 },
+        	{ topic: 'coordenadas', numPartitions: 2 }
+        ],
+});
+    await admin.disconnect();
+};
+
+topicParticiones().then(() => console.log('done'));
+
 const producer = kafka.producer();
 
 app.post("/login", async (req, res) => {
@@ -38,10 +54,17 @@ app.post("/RegistroMiembro", async (req, res) => {
     //console.log(req.body.user, "/", req.body.pass, req.body.time);
     //console.log(new Date(req.body.time).toLocaleDateString("es-CL"), new Date(req.body.time).toLocaleTimeString("es-CL"), req.body.user, "esta intentando ingresar.");
     await producer.connect();
-    await producer.send({
-        topic: 'ingreso',            //Ingreso Nuevos Miembros
-        messages: [{value: JSON.stringify(req.body)}]
-    })
+    if (req.body.premium == true) {
+    	await producer.send({
+        	topic: 'nuevosMiembros',            //Ingreso Nuevos Miembros Premium
+        	messages: [{key: 'ingresoUno', value: JSON.stringify(req.body), partition: 0}]
+    	})
+    } else {
+    	await producer.send({
+        	topic: 'nuevosMiembros',            //Ingreso Nuevos Miembros NO Premium
+        	messages: [{key: 'ingresoDos', value: JSON.stringify(req.body), partition: 1}]
+    	})
+    }
     await producer.disconnect().then(
         res.status(200).json({
             nombre: req.body.nombre,
@@ -65,6 +88,10 @@ app.post("/RegistroVenta", async (req, res) => {
         topic: 'stock',                                    //stock
         messages: [{value: JSON.stringify(req.body)}]
     })
+    await producer.send({
+        topic: 'coordenadas',                               //Registro de ubicación carrito
+        messages: [{key: 'coordenadasUno', value: JSON.stringify(req.body), partition: 0}]
+    })
     await producer.disconnect().then(
         res.status(200).json({
             cliente: req.body.cliente,
@@ -81,8 +108,8 @@ app.post("/AvisoAgenteExt", async (req, res) => {
     //console.log(new Date(req.body.time).toLocaleDateString("es-CL"), new Date(req.body.time).toLocaleTimeString("es-CL"), req.body.user, "esta intentando ingresar.");
     await producer.connect();
     await producer.send({
-        topic: 'avisos',                               //Avisos agentes externos
-        messages: [{value: JSON.stringify(req.body)}]
+        topic: 'coordenadas',                               //Avisos agentes externos, carrito prófugo
+        messages: [{{key: 'coordenadasDos', value: JSON.stringify(req.body), partition: 1}}]
     })
     await producer.disconnect().then(
         res.status(200).json({
