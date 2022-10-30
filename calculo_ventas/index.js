20baa7af93597ec123c5d2535474d7f576cceacc
GabrielRen21 <104105791+GabrielRen21@users.noreply.github.com>
2022-10-30 13:59:31 -0300

    Update index.js

--- calculo_ventas/index.js
+++ calculo_ventas/index.js
@@ -14,33 +14,38 @@ const kafka = new Kafka({
 
 function estadisticas(registro,resultados,carros){
 
-    registro.forEach( (value, key, map) => {
+    if (registro.size == 0) {
+		console.log("Ningún carro ha registrado ventas");
+	} else {
 
-        var contadorVentas = 0;
-        var clientes = new Set();
-        var cantidad = 0;
-        var aux = new Array;
+    	registro.forEach( (value, key, map) => {
+
+        	var contadorVentas = 0;
+        	var clientes = new Set();
+        	var cantidad = 0;
+        	var aux = new Array;
         
-        var ventasTotales = 0;
-        var promedioVentas = 0;
-        var clientesTotales = 0;
+        	var ventasTotales = 0;
+        	var promedioVentas = 0;
+        	var clientesTotales = 0;
         
             value.forEach(function(data) {
                 contadorVentas = contadorVentas + 1;
                 clientes.add(data.cliente);
                 cantidad = cantidad + parseInt(data.cantidad_sopaipillas);
             });
-        ventasTotales = contadorVentas;
-        clientesTotales = clientes.size;
-        promedioVentas = cantidad/clientesTotales;
+            
+        	ventasTotales = contadorVentas;
+        	clientesTotales = clientes.size;
+        	promedioVentas = cantidad/clientesTotales;
         
-        aux.push(ventasTotales);
-        aux.push(clientesTotales);
-        aux.push(promedioVentas);
-        resultados.set(key,aux);
-          });
+        	aux.push(ventasTotales);
+        	aux.push(clientesTotales);
+        	aux.push(promedioVentas);
+        	resultados.set(key,aux);
+        	
+		});
     
-
         resultados.forEach( (value, key, map) => {
 
             console.log("carro : ",key);
@@ -49,49 +54,36 @@ function estadisticas(registro,resultados,carros){
             console.log("clientes totales : ",aux[1]);
             console.log("promedio de ventas : ",aux[2]);
 
-              });
+		});
         
         carros.clear();
         registro.clear();
         resultados.clear();
-
+	}
 }
 
-/*
-await producer.connect();
-await producer.send({
-    topic: 'ingreso',            //Ingreso Nuevos Miembros
-    messages: [{value: JSON.stringify(req.body)}]
-})
-*/
 
-//var white_list = new Map();
 var lista_ventas = [];
-//var ventasTotales = 0;
-//var promedioVentas = 0;
-//var clientesTotales = 0;
 const carros = new Set();
-//const clientes = new Set();
 var registro = new Map();
 var resultados = new Map();
 
+setInterval(estadisticas, 3600000, registro, resultados, carros); //Número indica el tiempo en milisegundos 
 
-const ventas = async () => {
+const ventasUno = async () => {
     const consumer = kafka.consumer({ groupId: 'ventas', fromBeginning: true });
     await consumer.connect();
     await consumer.subscribe({ topic: 'ventas' });
     await consumer.run({
-        partitionsConsumedConcurrently: 2,
         eachMessage: async ({ topic, partition, message }) => {
-
             var particion = JSON.parse(partition);
 
             if (message.value){
-                if(particion==0){
+                /*if(particion==0){
                     console.log("Particion:",particion,"(balanceo de cargas)");
                 }else if(particion==1){
                     console.log("Particion:",particion,"(balanceo de cargas)");
-                }
+                }*/
                 var data = JSON.parse(message.value.toString());
                 //lista_ventas.push(data);
                 if(!carros.has(data.patente)){
@@ -104,17 +96,41 @@ const ventas = async () => {
                     aux.push(data);
                     registro.set(data.patente,aux);
                 }
-
-                display = setInterval(estadisticas, 60000, registro, resultados, carros);
-                //console.log(data);
-                
             }
         },
       })
 }
 
-//console.log("!registro!",registro);
+const ventasDos = async () => {
+    const consumer = kafka.consumer({ groupId: 'ventas', fromBeginning: true });
+    await consumer.connect();
+    await consumer.subscribe({ topic: 'ventas' });
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            var particion = JSON.parse(partition);
 
+            if (message.value){
+                /*if(particion==0){
+                    console.log("Particion:",particion,"(balanceo de cargas)");
+                }else if(particion==1){
+                    console.log("Particion:",particion,"(balanceo de cargas)");
+                }*/
+                var data = JSON.parse(message.value.toString());
+                //lista_ventas.push(data);
+                if(!carros.has(data.patente)){
+                    var aux = new Array;
+                    aux.push(data);
+                    carros.add(data.patente);
+                    registro.set(data.patente,aux);
+                }else{
+                    var aux = registro.get(data.patente);
+                    aux.push(data);
+                    registro.set(data.patente,aux);
+                }
+            }
+        },
+      })
+}
 
 
 app.get("/ventas", async (req, res) => {
@@ -167,5 +183,6 @@ app.get("/ventas", async (req, res) => {
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-    ventas();
+    ventasUno();
+    ventasDos();
 });
